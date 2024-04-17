f758b75cc14c7cd9085c52e8c221e131f95de3e6
lautaro <acostaquintalautaro@gmail.com>
2024-04-17 11:58:36 -0300

    Mejoré mucho el tiempo implementando la suma de los divisores mediante factorización

--- 20_millones.py
+++ 20_millones.py
@@ -1,31 +1,29 @@
+# https://wiki.python.org/moin/TimeComplexity
 from helpers import timer
-cache = {}
-cache_hits = 0
 
-
-def potencia_por_cuadrados(base: int, e: int, mod: int) -> int:
+def potencia_por_cuadrados(base: int, exp: int, mod: int) -> int:
     """
-        Calcula a^n en O(log n) multiplicaciones.
+    Calcula base^n en O(log n) multiplicaciones.
 
-        Referencias:
-        https://cp-algorithms.com/algebra/binary-exp.html
-        https://es.wikipedia.org/wiki/Exponenciaci%C3%B3n_binaria
+    Referencias:
+    https://cp-algorithms.com/algebra/binary-exp.html
+    https://es.wikipedia.org/wiki/Exponenciaci%C3%B3n_binaria
     """
     result = 1
     base %= mod
-    while e:
-        if e & 1:
+    while exp:
+        if exp & 1:
             result = result * base % mod
         base = base * base % mod
-        e >>= 1
+        exp >>= 1
 
     return result
 
 
 def es_compuesto(num: int, a: int, d: int, s: int) -> bool:
     """
-        Referencias:
-        https://cp-algorithms.com/algebra/primality_tests.html
+    Referencias:
+    https://cp-algorithms.com/algebra/primality_tests.html
     """
     x = potencia_por_cuadrados(a, d, num)
     if x == 1 or x == (num - 1):
@@ -38,13 +36,14 @@ def es_compuesto(num: int, a: int, d: int, s: int) -> bool:
     return True
 
 
+@timer
 def miller_rabin_deterministico(num: int) -> bool:
     """
-        Versión determinística del algoritmo del test de primalidad de Miller-Rabin
+    Versión determinística del algoritmo del test de primalidad de Miller-Rabin
 
-        Referencias:
-        https://cp-algorithms.com/algebra/primality_tests.html
-        https://es.wikipedia.org/wiki/Test_de_primalidad
+    Referencias:
+    https://cp-algorithms.com/algebra/primality_tests.html
+    https://es.wikipedia.org/wiki/Test_de_primalidad
     """
     if num < 2:
         return False
@@ -65,13 +64,14 @@ def miller_rabin_deterministico(num: int) -> bool:
 
     return True
 
+
 @timer
 def division_tentativa(num: int) -> list[int]:
     """
-        El algoritmo más básico para factorizar un entero en números primos
+    El algoritmo más básico para factorizar un entero en números primos
 
-        Referencias:
-        https://cp-algorithms.com/algebra/factorization.html
+    Referencias:
+    https://cp-algorithms.com/algebra/factorization.html
     """
 
     factores = []
@@ -88,45 +88,52 @@ def division_tentativa(num: int) -> list[int]:
 
     return factores
 
+@timer
 def suma_de_factores_propios_factorizado(num: int) -> int:
     """
-    1) Obtener los factores a través de una tecnica de factorización.
-    2) Encontrar todas las combinaciones de los factores con sus exponentes.
-    3) Realizar la sumatoria de los resultados de cada combinación excepto la primera.
+        1) Obtener los factores a través de una tecnica de factorización.
+        2) Encontrar todas las combinaciones de los factores con sus exponentes.
+        3) Realizar la sumatoria de los resultados de cada combinación excepto la primera.
+
+        Referencias:
+        - https://planetmath.org/formulaforsumofdivisors
     """
 
-    result = 0
-
     factores = division_tentativa(num)
-    # TODO: implementar cómo encontrar todas las combinaciones de factores.
-    return result
+    numeros = {}
+    for n in factores:
+        numeros[n] = numeros.get(n, 0) + 1 
+
+    exponentes = list(numeros.values())
+    numeros_unicos = list(numeros.keys())
+
+    result = 1
+    for n, e in zip(numeros_unicos, exponentes):
+        result *= (pow(n, e+1) - 1) / (n-1)
+    
+    return int(result - num)
+
 
 @timer
 def suma_de_factores_propios_fuerza_bruta(num: int) -> int:
     """
     Esta es la solución más facil pero también la más lenta.
     """
-    # TODO: Revisar si introducir un caché sirve de algo para este tipo de problemas.
-    if num in cache:
-        global cache_hits
-        cache_hits += 1
-        return cache[num]
-
     result = 0
-    for i in range(1, num - 1):
+    for i in range(1, num):
         if num % i == 0:
             result += i
 
-    cache[num] = result
     return result
+
 
 def construir_sucesion(start: int, num: int, arr: list[int]) -> tuple[bool, list[int]]:
     """
-        Devuelve verdadero o falso dependiendo si la sucesión, en donde cada término es la suma 
-        de los divisores propios del término anterior, es infinita.
+    Devuelve verdadero o falso dependiendo si la sucesión, en donde cada término es la suma
+    de los divisores propios del término anterior, es infinita.
     """
-    sum = suma_de_factores_propios_fuerza_bruta(num)
-    # sum = suma_de_factores_propios_factorizado(num)
+    # sum = suma_de_factores_propios_fuerza_bruta(num)
+    sum = suma_de_factores_propios_factorizado(num)
 
     # Si es así, significa que se cumplió el periodo, entonces devuelvo la lista.
     if sum == start:
@@ -140,16 +147,17 @@ def construir_sucesion(start: int, num: int, arr: list[int]) -> tuple[bool, list
     arr.append(sum)
     return construir_sucesion(start, sum, arr)
 
+
 @timer
 def serie_de_numeros_sociables(num: int, arr: list[int]) -> tuple[bool, list[int]]:
     """
-        Realiza un control de si el número es primo para así ahorrar operaciones.
+    Realiza un control de si el número es primo para así ahorrar operaciones.
 
-        De no ser primo, buscará encontrar si el número tiene una sucesión alícuota.
+    De no ser primo, buscará encontrar si el número tiene una sucesión alícuota.
 
-        Referencias:
-        - https://es.wikipedia.org/wiki/N%C3%BAmeros_sociables
-        - https://es.wikipedia.org/wiki/Sucesi%C3%B3n_al%C3%ADcuota
+    Referencias:
+    - https://es.wikipedia.org/wiki/N%C3%BAmeros_sociables
+    - https://es.wikipedia.org/wiki/Sucesi%C3%B3n_al%C3%ADcuota
 
     """
     if miller_rabin_deterministico(num):
@@ -162,7 +170,7 @@ def serie_de_numeros_sociables(num: int, arr: list[int]) -> tuple[bool, list[int
 if __name__ == "__main__":
     # Según wikipedia, el número sociable más pequeño es el 12_496
     # TODO: Ver cómo llega a verificar esto, así si lo podemos implementar podemos no gastarnos en verificar números a la fuerza.
-    for num in range(1, int(20*10e6)):
+    for num in range(1, int(20 * 10e6)):
         print(f">>{num}")
         candidato, arr = serie_de_numeros_sociables(num, [])
         if candidato:
