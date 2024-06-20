11db84ffb277268eb783fc07058648f079670025
lautaro acosta <acostaquintanalautaro@gmail.com>
2024-06-20 15:25:30 -0300

    Uni los dos programas

--- TPI/trabajo.py
+++ TPI/trabajo.py
@@ -1,6 +1,49 @@
 import sys
 
+# Un viajante tiene que llegar de la ciudad A a la ciudad B, con un tanque de nafta de tamaño C,
+# que es la maxima capacidad en litros del tanque de combustible. Entre la ciudad A y la ciudad B hay N estaciones de servicio,
+# cada una con un precio de nafta asociado, y en cada estacion de servicio el viajante puede llenar el tanque.
+# Sabemos la distancia que hay entre la ciudad A y la primera estacion de servicio, entre la ciudad B y la estacion de servicio N,
+# y entre cada una de las estaciones de servicio. El viajante quiere parar lo menos posible a cargar nafta y parar
+# en las estaciones más baratas, por lo tanto, queremos minimizar la cantidad de paradas y el gasto al minimo posible para llegar de la ciudad A a la ciudad B.
+
 CAPACIDAD_TANQUE = 100
+
+# Heuristica: Desde la posición actual busco cual es la estación más lejana que puedo alcanzar
+# con la capacidad de mi tanque. Entre las estaciónes dentro de ese alcance, selecciono la que tiene el costo minimo
+def min_paradas_min_costo(
+    opciones: list[tuple[int,int]]
+) -> tuple[int, float]:
+    pos_actual = 0
+    cant_paradas = 0
+    costo_total = 0
+    n = len(opciones)
+
+    while pos_actual < n - 1:
+        costo_min = float("inf")
+        sig_pos = 0
+
+        for i in range(pos_actual + 1, n):
+            if opciones[i][0] - opciones[pos_actual][0] <= CAPACIDAD_TANQUE:
+                if opciones[i][1] <= costo_min:
+                    costo_min = opciones[i][1]
+                    sig_pos = i
+            else:
+                # En este punto todo el resto de distancias ya estarían fuera de mi alcance.
+                break
+
+        # Si no es el final, entonces paré en una estación
+        if sig_pos < n - 1:
+            cant_paradas += 1
+            print(f"parada: {opciones[sig_pos][0]}, costo: {costo_min}")
+            costo_total += (
+                costo_min * (opciones[sig_pos][0] - opciones[pos_actual][0]) / CAPACIDAD_TANQUE
+            )
+
+        pos_actual = sig_pos
+
+    return cant_paradas, costo_total
+
 
 def backtracking(
     idx: int,
@@ -24,7 +67,7 @@ def backtracking(
         return mejor_gasto, mejor_cantidad, mejor_seleccion, capacidad_en_mejor_caso
 
     distancia = opciones[idx + 1][0] - opciones[idx][0]
-    print(f"La distancia entre {opciones[idx + 1][0]} y {opciones[idx][0]} es: {distancia} y mi capacidad es: {capacidad_tanque}")
+    # print(f"La distancia entre {opciones[idx + 1][0]} y {opciones[idx][0]} es: {distancia} y mi capacidad es: {capacidad_tanque}")
     # TODO: Revisar el bug, en donde cuando retrocede despues de verificar que no se puede tomar esa ruta
     # añade a la lista de paradas seleccionadas, la parada en donde decide que va a cargar en la siguiente parada y no a dicha siguiente parada. 
     # Si probas con una capacidad de 100, va a devolverte [(50, 100), (180, 50)] cuando debería devolverte [(100, 60), (200, 100)]
@@ -78,6 +121,13 @@ def main():
         (250, 50),
         (300, 0),
     ]
+
+    print("------------------------------\n\tGREEDY\n------------------------------ ")
+    paradas, total_cost = min_paradas_min_costo(estaciones)
+    print(f"Numero de paradas: {paradas}, Costo total: {total_cost}")
+
+
+    print("------------------------------\n\tBACKTRACKING\n------------------------------ ")
     mejor_gasto, mejor_cantidad, mejor_seleccion, capacidad_tanque = backtracking(
         0,
         estaciones,
@@ -90,12 +140,12 @@ def main():
         sys.maxsize,
         0,
     )
-    print(f"Mejor gasto: {mejor_gasto}")
-    print(f"Mejor_cantidad: {mejor_cantidad}")
+    print(f"Numero de paradas: {mejor_cantidad}")
+    print(f"Costo total: {mejor_gasto}")
     print(
         f"Capacidad final del tanque: {capacidad_tanque}"
-    )  # FIXME: Revisar porque tendría que ser 130 no 100.
-    print(f"Mejor_seleccion: {mejor_seleccion}")
+    )   
+    print(f"Paradas tomadas: {mejor_seleccion}")
 
 
 if __name__ == "__main__":
